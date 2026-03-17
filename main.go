a6fb733df9c1a7e3462cffe38793bc91d75e5054
JakubLipnicki <jakublipnicki1@gmail.com>
2026-03-17 07:33:32 +0100

    example weather server

--- main.go
+++ main.go
@@ -49,12 +49,183 @@ type AlertFeature struct {
 
 type AlertProperties struct {
 	Event       string `json:"event"`
-	AreaDesc    string `json:areaDesc`
-	Severity    string `json:"severity`
+	AreaDesc    string `json:"areaDesc"`
+	Severity    string `json:"severity"`
 	Descritpion string `json:"description"`
 	Instruction string `json:"Instruction"`
 }
 
+type ForecastInput struct {
+	Latitude  float64 `json:"latitude" jsonschema:"Latitude of the location"`
+	Longitude float64 `json:"longitude" jsonschema:"Longitude of the location"`
+}
+
+type AlertsInput struct {
+	State string `json:"state" jsonschema:"Two-letters US state code (e.g. CA, NY)"`
+}
+
+func makeNWSRequest[T any](ctx context.Context, url string) (*T, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w, err")
+	}
+
+	req.Header.Set("User-Agent", UserAgent)
+	req.Header.Set("Accept", "application/geo+json")
+
+	client := http.DefaultClient
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make request to %s: %w", url, err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return nil, fmt.Errorf("HTTP error %d, %s", resp.StatusCode, string(body))
+	}
+
+	var result T
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return &result, nil
+}
+
+func formatAlert(alert AlertFeature) string {
+	props := alert.Properies
+	event := cmp.Or(props.Event, "Unknown")
+	areaDesc := cmp.Or(props.AreaDesc, "Unknown")
+	severity := cmp.Or(props.Severity, "Unknown")
+	description := cmp.Or(props.Descritpion, "No description available")
+	instuction := cmp.Or(props.Instruction, "No specific insturtions provided")
+
+	return fmt.Sprintf(`
+	Event: %s
+	Area: %s
+	Severity: %s
+	Descritpion: %s
+	Instruction: %s
+	`, event, areaDesc, severity, description, &instuction)
+}
+
+func formatPeriod(period ForecastPeriod) string {
+	return fmt.Sprintf(`
+	%s:
+	Temperature: %d°%s
+	Wind: %s %s
+	ForecastL: %s
+	`, period.Name, period.Temperature, period.TemperatureUnit, period.WindSpeed, period.WindDirection, period.DetailedForecast)
+}
+
+func getForecast(ctx context.Context, req *mcp.CallToolRequest, input ForecastInput) (
+	*mcp.CallToolResult, any, error,
+) {
+	pointsURL := fmt.Sprintf("%s/points/%f,%f", NWSAPIBase, input.Latitude, input.Longitude)
+	pointsData, err := makeNWSRequest[PointsResponse](ctx, pointsURL)
+	if err != nil {
+		return &mcp.CallToolResult{
+			Content: []mcp.Content{
+				&mcp.TextContent{Text: "Unable to fetch forecast data for this location"},
+			},
+		}, nil, nil
+	}
+	forecastURL := pointsData.Properties.Forecast
+	if forecastURL == "" {
+		return &mcp.CallToolResult{
+			Content: []mcp.Content{
+				&mcp.TextContent{Text: "Unable to fetch forecastURL"},
+			},
+		}, nil, nil
+	}
+
+	forecastData, err := makeNWSRequest[ForecastResponse](ctx, forecastURL)
+	if err != nil {
+		return &mcp.CallToolResult{
+			Content: []mcp.Content{
+				&mcp.TextContent{Text: "Unable to fetch detailed forecast."},
+			},
+		}, nil, nil
+	}
+
+	periods := forecastData.Properties.Periods
+	if len(periods) == 0 {
+		return &mcp.CallToolResult{
+			Content: []mcp.Content{
+				&mcp.TextContent{Text: "No forecast periods available."},
+			},
+		}, nil, nil
+	}
+
+	var forecasts []string
+	for i := range min(5, len(periods)) {
+		forecasts = append(forecasts, formatPeriod(periods[i]))
+	}
+
+	result := strings.Join(forecasts, "\n---\n")
+
+	return &mcp.CallToolResult{
+		Content: []mcp.Content{
+			&mcp.TextContent{Text: result},
+		},
+	}, nil, nil
+}
+
+// Alerts
+func getAlerts(ctx context.Context, req *mcp.CallToolRequest, input AlertsInput) (*mcp.CallToolResult, any, error) {
+	stateCode := strings.ToUpper(input.State)
+	alertsURL := fmt.Sprintf("%s/alerts/active/area/%s", NWSAPIBase, stateCode)
+
+	alertsData, err := makeNWSRequest[AlertResponse](ctx, alertsURL)
+	if err != nil {
+		return &mcp.CallToolResult{
+			Content: []mcp.Content{
+				&mcp.TextContent{Text: "Unable to fetch alerts or no alerts found."},
+			},
+		}, nil, nil
+	}
+
+	if len(alertsData.Features) == 0 {
+		return &mcp.CallToolResult{
+			Content: []mcp.Content{
+				&mcp.TextContent{Text: "No active alerts for this state."},
+			},
+		}, nil, nil
+	}
+
+	var alerts []string
+	for _, feature := range alertsData.Features {
+		alerts = append(alerts, formatAlert(feature))
+	}
+
+	result := strings.Join(alerts, "\n---\n")
+	return &mcp.CallToolResult{
+		Content: []mcp.Content{
+			&mcp.TextContent{Text: result},
+		},
+	}, nil, nil
+
+}
+
 func main() {
-	fmt.Println("test")
+	server := mcp.NewServer(&mcp.Implementation{
+		Name:    "weather",
+		Version: "1.0.0",
+	}, nil)
+
+	mcp.AddTool(server, &mcp.Tool{
+		Name:        "get_forecast",
+		Description: "Get weather forecast for a location",
+	}, getForecast)
+
+	mcp.AddTool(server, &mcp.Tool{
+		Name:        "get_alerts",
+		Description: "Get weather alerts for a US state",
+	}, getAlerts)
+
+	if err := server.Run(context.Background(), &mcp.StdioTransport{}); err != nil {
+		log.Fatal(err)
+	}
 }
